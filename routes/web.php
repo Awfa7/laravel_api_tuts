0be3138ee7faae6701032f8b3d93c275eb8179db
awfa7 <awfa1996@gmail.com>
2022-08-01 20:28:11 +0300

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup' , function() {
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+    ];
+
+    if(!Auth::attempt($credentials)){
+        $user = new \App\Models\User();
+
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+
+        if(Auth::attempt($credentials)) {
+            /** @var \App\Models\User $user **/
+            $user = Auth::user();
+
+            $adminToken = $user->createToken('adminToken' , ['create','update','delete']);
+            $updateToken = $user->createToken('updateToken', ['create','update']);
+            $basicToken = $user->createToken('basic-Token');
+
+            return [
+                'admin' => $adminToken->plainTextToken,
+                'update' => $updateToken->plainTextToken,
+                'basic' => $basicToken->plainTextToken
+            ];
+        }
+    }
 });
